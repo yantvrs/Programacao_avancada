8dca437785ac957df62a69679113ba86b570367a
yantavares2002 <yantavares2002@gmail.com>
2022-04-21 16:23:46 -0300

    Questão 1

--- questao_01.c
+++ questao_01.c
@@ -1,6 +1,17 @@
 # include<stdio.h>
 
 int main(){
-    printf("jfljajajl");
+	
+	//1. Seja o seguinte trecho de programa:
+    int i=3,j=5;
+	int *p, *q;
+	p = &i;
+	q = &j;
+	//Determine o valor das seguintes expressões:
+	printf("%d = Verdade\n",p == &i);
+	printf("%d\n", *p - *q);
+	printf("%d\n", **&p);
+	printf("%d\n", 3 - *p/(*q) + 7);
+	
     return 0;
 }
