24c5146ebb75a36fb2cfffcd03bedf6e6ae7fd31
yantavares2002 <yantavares2002@gmail.com>
2022-04-21 16:45:54 -0300

    Questão 2

---
+++ questao_02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+	//2. Mostre o que será impresso por programa supondo que i ocupa o endereço 4094 na memória.
+	
+	int i=5, *p;
+	p = &i;
+	printf("%x, %d, %d, %d, %d", p,*p+2,**&p,3**p,**&p+4);
+
+	return 0;
+}
