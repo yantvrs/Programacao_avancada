5b04adcb276e3d06b37fee6dd88e8e1cb03d9ff8
yantavares2002 <yantavares2002@gmail.com>
2022-04-21 17:27:10 -0300

    Questão 3

---
+++ questão03.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	//3. Se i e j são variáveis inteiras e p e q ponteiros para int, 
+	//quais das seguintes expressões de atribuição são ilegais?
+	int *i, *j;
+	int *p, *q;
+	
+	p = i; 
+	q = &j;
+	p = &*&i;
+	i = (*&)j;
+	i = *&j;
+	i = *&*&j;
+	q = *p;
+	i = (*p)++ + *q;
+	printf("%d",q);
+	return 0;
+}
