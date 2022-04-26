e033f25f4656e221eaddf4fa71c15d01e256fd48
yantavares2002 <yantavares2002@gmail.com>
2022-04-26 17:35:56 -0300

    Questão 1 da primeira prova

---
+++ Questões da prova 1/Questao_01_prova_01.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//QUESTÃO 1
+//1)Alocar a matriz 3d com dimensões dadas pelo usuário;
+//2)Desenhar um elipsóide com centre coincidente com o centro do paralelepípedo com raios determinados pelo usuário;
+//3)Imprimir na tela um dos planos que o usuário escolher;
+
+int main() {
+  int i, j, k;
+  int nl, nc, np;
+  int a,b,c;
+  int plano;
+
+  float centro_i,centro_j,centro_k;
+  float elips;
+  
+  //Paralelepípedo
+  //Linhas
+  printf("Primeiramente, informe as dimensões do paralelepípedo... \n");
+  printf("Linhas: ");
+  scanf("%d",&nl);
+  //Colunas
+  printf("Colunas: ");
+  scanf("%d",&nc);
+  //Planos
+  printf("Planos: ");
+  scanf("%d",&np);
+
+  //Elipsóide
+  //Raio a 
+  printf("Agora, informe as dimensões dos raios do elipsóide... \n");
+  printf("A: ");
+  scanf("%d",&a);
+  ///Raio b
+  printf("B: ");
+  scanf("%d",&b);
+  //Raio c
+  printf("C: ");
+  scanf("%d",&c);
+
+  //Plano que o usuário irá pedir para impressão na tela
+  printf("Informe o plano que será impresso: ");
+  scanf("%d",&plano);
+
+  //Alocação
+  int ***cubo = (int ***)calloc(np,sizeof(int**));
+  //Planos
+  for(k=0;k<np;k++){
+    cubo[k] = (int**)calloc(nl, sizeof(int*));
+    //Linhas
+    for(i=0;i<nl;i++){
+      cubo[k][i] = (int*)calloc(nc, sizeof(int));
+    }
+  }
+  
+  //Cálculo dos centros do paralelepípedo
+  centro_i = nl/2;
+  centro_j = nc/2;
+  centro_k = np/2;
+
+  //Alocando elipsóide no paralelepípedo
+  for(k=0;k<np;k++){
+    for(i=0;i<nl;i++){
+      for(j=0;j<nc;j++){
+
+        //Fórmula do elipsóide com preenchimento interno
+        elips = (((i-centro_i)/a)*((i-centro_i)/a))+(((j-centro_j)/b)*((j-centro_j)/b))+(((k-centro_k)/c)*((k-centro_k)/c));
+        
+        //Condições para o preenchimento das elipses 
+        if(elips<=1){
+          cubo[k][i][j]=1; //Para construir o elipsóide
+        }
+        else{
+          cubo[k][i][j]=0; //Para preencher os lugares vazios
+        }
+        elips = 0;
+      }
+    }
+  }
+
+  /*
+  //Print de todos os planos
+  for(k=0;k<np;k++){
+    for(i=0;i<nl;i++){
+      for(j=0;j<nc;j++){
+        printf("%d ",cubo[k][i][j]);
+      }
+      printf("\n");
+    }
+    printf("\n");
+  }
+  */
+  
+  //Print do plano dado
+  printf("Plano %d\n",plano);
+  for(i=0;i<nl;i++){
+    for(j=0;j<nc;j++){
+      printf("%d ",cubo[plano][i][j]);
+    }
+    printf("\n");
+  }
+  //Liberando a memória alocada 
+  for(i =0;i<nl;i++){
+    free(cubo[i]);
+  }
+  free(cubo);
+  cubo = NULL;
+
+  
+  return 0;
+}
