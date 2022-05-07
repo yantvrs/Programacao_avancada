dc30aff54a7171db79b32ecb70a29e6f181de105
yantavares2002 <yantavares2002@gmail.com>
2022-05-07 19:21:29 -0300

    Definição de classes em c++

--- Unidade 2/Projeto_teste/main.cpp
+++ Unidade 2/Projeto_teste/main.cpp
@@ -16,6 +16,25 @@ using namespace std;
  * Código fonte ------compilação----->Código Objeto-->/
  * [.cpp,.hpp]    (g++ -c code3.cpp)      [.o]
 
+
+ Obs.: Torna-se mais vantajoso separar por partes essa função, pois o processo de compilação
+       não é muito pesado para a máquina.
+
+ >>O que são classes? São grupos de objetos que compartilham características comuns.
+
+  Objeto real: entidade tangível que apresenta algum coportamento bem definido.
+*Todo objeto possui:
+  Identidade: é a propriedade que diferencia os objetos uns dos outros.
+  Comportamento: define o que ele faz ou como reage perante mudanças.
+  Estado: define as propredades do objeto, bem como seus valores.
+
+*Objetos digitais também possuem as mesmas propriedades.
+  Identidade: posição única na memória.
+  Comportamento: funcionalidades implementadas.
+  Estado: valores de variáveis.
+
+>>O que é um objeto? É uma instância de uma classe.
+
 */
 
 int main()
