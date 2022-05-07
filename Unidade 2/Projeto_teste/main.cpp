c9dacb87fabf60630e48328a36d84353f2298641
yantavares2002 <yantavares2002@gmail.com>
2022-05-07 15:47:15 -0300

    Atualizações

--- Unidade 2/Projeto_teste/main.cpp
+++ Unidade 2/Projeto_teste/main.cpp
@@ -4,6 +4,20 @@
 
 using namespace std;
 
+//Notas de aula:
+
+/*
+ * Código fonte ------compilação----->Código Objeto--> + --->linkedição-->Executável
+ * [.cpp,.hpp]    (g++ -c code1.cpp)      [.o]         |     (g++ + code1.o + code2.o + code2.o + -o exec)
+ *                                                     |
+ * Código fonte ------compilação----->Código Objeto--->|
+ * [.cpp,.hpp]    (g++ -c code2.cpp)      [.o]         |
+ *                                                     |
+ * Código fonte ------compilação----->Código Objeto-->/
+ * [.cpp,.hpp]    (g++ -c code3.cpp)      [.o]
+
+*/
+
 int main()
 {
     std::cout << "Alo, C++!\n";
